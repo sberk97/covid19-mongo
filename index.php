ef04aebb4072772e7a98324ec0312c4c207d52c0
sberk97 <sebastian.berk97@gmail.com>
2020-11-20 23:55:11 +0100

    Created left side bar with selectable column names

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+  <title>COVID-19 Database</title>
+  <meta name="author" content="Sebastian Berk">
+  <link rel="stylesheet" type="text/css" href="styles.css">
+  <script src="js/script.js" async defer></script>
+</head>
+<body>
+  <div class="sidenav">
+    <div class="column-description">Click to select a column</div>
+    <label><input type="checkbox" class="column-checkbox" /><span>FIPS</span></label>
+    <label><input type="checkbox" class="column-checkbox" /><span>Admin2</span></label>
+    <label><input type="checkbox" class="column-checkbox" /><span>Province State</span></label>
+    <label><input type="checkbox" class="column-checkbox" /><span>Country Region</span></label>
+    <label><input type="checkbox" class="column-checkbox" /><span>Last Update</span></label>
+    <label><input type="checkbox" class="column-checkbox" /><span>Latitude</span></label>
+    <label><input type="checkbox" class="column-checkbox" /><span>Longitude</span></label>
+    <label><input type="checkbox" class="column-checkbox" /><span>Confirmed</span></label>
+    <label><input type="checkbox" class="column-checkbox" /><span>Deaths</span></label>
+    <label><input type="checkbox" class="column-checkbox" /><span>Recovered</span></label>
+    <label><input type="checkbox" class="column-checkbox" /><span>Active</span></label>
+    <label><input type="checkbox" class="column-checkbox" /><span>Combined Key</span></label>
+    <label><input type="checkbox" class="column-checkbox" /><span>Incidence Rate</span></label>
+    <label><input type="checkbox" class="column-checkbox" /><span>Case-Fatality Ratio</span></label>
+  </div>
+
+  <div class="main">
+    <h2>Sidenav Example</h2>
+    <p>This sidenav is always shown.</p>
+  </div>
+</body>
+</html>
+
+<!-- <?php
+print phpinfo();
+?> -->
