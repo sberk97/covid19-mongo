17c8864edd819253437ceaf59f9d99c4e715ffa9
sberk97 <sebastian.berk97@gmail.com>
2020-11-22 19:45:07 +0100

    Display fields based on checkboxes

--- index.php
+++ index.php
@@ -12,27 +12,33 @@
 <body>
   <div class="sidenav">
     <div class="column-description">Click to select a column</div>
-    <input id="FIPS" type="checkbox" name="column-checkbox"/><label for="FIPS">FIPS</label>
-    <input id="Admin2" type="checkbox" name="column-checkbox"/><label for="Admin2">Admin2</label>
-    <input id="Province State" type="checkbox" name="column-checkbox"/><label for="Province State">Province State</label>
-    <input id="Country Region" type="checkbox" name="column-checkbox"/><label for="Country Region">Country Region</label>
-    <input id="Last Update" type="checkbox" name="column-checkbox"/><label for="Last Update">Last Update</label>
-    <input id="Latitude" type="checkbox" name="column-checkbox"/><label for="Latitude">Latitude</label>
-    <input id="Longitude" type="checkbox" name="column-checkbox"/><label for="Longitude">Longitude</label>
-    <input id="Confirmed" type="checkbox" name="column-checkbox"/><label for="Confirmed">Confirmed</label>
-    <input id="Deaths" type="checkbox" name="column-checkbox"/><label for="Deaths">Deaths</label>
-    <input id="Recovered" type="checkbox" name="column-checkbox"/><label for="Recovered">Recovered</label>
-    <input id="Active" type="checkbox" name="column-checkbox"/><label for="Active">Active</label>
-    <input id="Combined Key" type="checkbox" name="column-checkbox"/><label for="Combined Key">Combined Key</label>
-    <input id="Incidence Rate" type="checkbox" name="column-checkbox"/><label for="Incidence Rate">Incidence Rate</label>
-    <input id="Case-Fatality Ratio" type="checkbox" name="column-checkbox"/><label for="Case-Fatality Ratio">Case-Fatality Ratio</label>
+    <input id="fips" type="checkbox" name="column-checkbox"/><label for="fips">FIPS</label>
+    <input id="admin2" type="checkbox" name="column-checkbox"/><label for="admin2">Admin2</label>
+    <input id="province-state" type="checkbox" name="column-checkbox"/><label for="province-state">Province State</label>
+    <input id="country-region" type="checkbox" name="column-checkbox"/><label for="country-region">Country Region</label>
+    <input id="last-update" type="checkbox" name="column-checkbox"/><label for="last-update">Last Update</label>
+    <input id="latitude" type="checkbox" name="column-checkbox"/><label for="latitude">Latitude</label>
+    <input id="longitude" type="checkbox" name="column-checkbox"/><label for="longitude">Longitude</label>
+    <input id="confirmed" type="checkbox" name="column-checkbox"/><label for="confirmed">Confirmed</label>
+    <input id="deaths" type="checkbox" name="column-checkbox"/><label for="deaths">Deaths</label>
+    <input id="recovered" type="checkbox" name="column-checkbox"/><label for="recovered">Recovered</label>
+    <input id="active" type="checkbox" name="column-checkbox"/><label for="active">Active</label>
+    <input id="combined-key" type="checkbox" name="column-checkbox"/><label for="combined-key">Combined Key</label>
+    <input id="incidence-rate" type="checkbox" name="column-checkbox"/><label for="incidence-rate">Incidence Rate</label>
+    <input id="case-fatality-ratio" type="checkbox" name="column-checkbox"/><label for="case-fatality-ratio">Case-Fatality Ratio</label>
     <button class="select-btn" onClick="selectAll();">Select all</button>
     <button class="select-btn" onClick="deselect();">Deselect</button>
   </div>
 
   <div class="main">
-    <h2>Sidenav Example</h2>
-    <p>This sidenav is always shown.</p>
+    <div id="fips-div" style="display:none;">
+      <label for="fips">FIPS:</label>
+      <input type="text" id="fips-input" name="fips">
+    </div>
+    <div id="admin2-div" style="display:none;">
+      <label for="admin2">Admin2:</label>
+      <input type="text" id="admin2-input" name="admin2">
+    </div>
   </div>
 </body>
 </html>
--- script.js
+++ script.js
@@ -12,3 +12,22 @@ function deselect() {
         checkboxes[i].checked = false;
     }
 }
+
+// $("#fips").change(function(event){
+//     console.log(this.id);
+//     if($(this).is(':checked')){
+//          $("#fips-div").show();
+//      } else {
+//          $("#fips-div").hide();
+//      }
+//  });
+
+ $("#fips, #admin2").change(changeVisibilityOfInput);
+
+ function changeVisibilityOfInput() {
+    if($(this).is(':checked')){
+        $("#" + this.id + "-div").show();
+    } else {
+        $("#" + this.id + "-div").hide();
+    }
+}
